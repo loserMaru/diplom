1752539d9923c225d97231b0e2ba094deaa57d07
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-24 00:48:27 +0300

    Main file update (app on event changed to lifespan)

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.db.base import Base
+from app.db.session import engine
+from app.db.init_db import init_models
+
+from app.core.config import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_models()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(title=settings.app_name, lifespan=lifespan)
