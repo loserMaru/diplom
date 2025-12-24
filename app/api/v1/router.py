74e87fcffd2c451a921b29e35d2da27ead5526ff
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-24 17:48:52 +0300

    Common router file update

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from app.api.v1.endpoints import users, auth
+
+api_router = APIRouter()
+
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
