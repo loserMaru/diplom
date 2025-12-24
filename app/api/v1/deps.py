5436284080da45d4f21a8f16d43adc361365fe5d
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-24 17:48:05 +0300

    Deps update

--- app/api/v1/deps.py
+++ app/api/v1/deps.py
@@ -1,6 +1,51 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from app.core.config import settings
 from app.db.session import AsyncSessionLocal
+from app.models.user import User
 
 
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+
+async def get_current_user(
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(oauth2_scheme),
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            settings.secret_key,
+            algorithms=["HS256"],
+        )
+        user_id: str | None = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    result = await db.execute(
+        select(User).where(User.id == int(user_id))
+    )
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise credentials_exception
+
+    return user
