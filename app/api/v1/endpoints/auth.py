314f15038cf4a42b457e656c2a9c0186eed32339
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-24 17:47:42 +0300

    Login and register

---
+++ app/api/v1/endpoints/auth.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from app.api.v1.deps import get_db
+from app.core.config import settings
+from app.core.security import verify_password, create_access_token
+from app.crud import user as crud_user
+from app.schemas.user import UserPublic, UserCreate
+
+router = APIRouter()
+
+
+@router.post("/login")
+async def login(
+        db: AsyncSession = Depends(get_db),
+        form_data: OAuth2PasswordRequestForm = Depends(),
+):
+    user = await crud_user.get_by_email(db, email=form_data.username)
+
+    if not user or not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    access_token = create_access_token(
+        subject=user.email,
+        expires_delta=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/register", response_model=UserPublic, status_code=status.HTTP_201_CREATED,)
+async def register(
+        user_in: UserCreate,
+        db: AsyncSession = Depends(get_db),
+):
+    existing_user = await crud_user.get_by_email(db, str(user_in.email))
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    user = await crud_user.create(db, data=user_in)
+    return user
