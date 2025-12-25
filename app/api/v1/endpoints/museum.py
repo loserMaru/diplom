fbaf54585662a57aa72baf94513255a347af20e6
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-25 23:10:04 +0300

    Museum endpoint update

--- app/api/v1/endpoints/museum.py
+++ app/api/v1/endpoints/museum.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from app.api.v1.deps import get_db
+from app.crud.base import create_with_relations
+from app.models.exhibit import Exhibit
 from app.models.museum import Museum
 from app.schemas.museum import MuseumCreate, MuseumPublic
 
@@ -12,28 +14,27 @@ router = APIRouter()
 
 @router.post("/", response_model=MuseumPublic)
 async def create_museum(
-    data: MuseumCreate,
-    db: AsyncSession = Depends(get_db),
+        data: MuseumCreate,
+        db: AsyncSession = Depends(get_db),
 ):
-    museum = Museum(
-        name=data.name,
-        description=data.description,
+    return await create_with_relations(
+        db=db,
+        model=Museum,
+        data=data,
+        load=Museum.images,
     )
-    db.add(museum)
-    await db.commit()
 
-    stmt = (
-        select(Museum)
-        .where(Museum.id == museum.id)
-        .options(selectinload(Museum.images))
-    )
-    result = await db.execute(stmt)
-    museum = result.scalar_one()
-
-    return museum
 
 @router.get("/", response_model=list[MuseumPublic])
 async def get_museums(db: AsyncSession = Depends(get_db), skip: int = 0, limit: int = 100):
-    stmt = (select(Museum).offset(skip).limit(limit).options(selectinload(Museum.images)))
+    stmt = (
+        select(Museum)
+        .offset(skip)
+        .limit(limit)
+        .options(
+            selectinload(Museum.images),
+            selectinload(Museum.exhibits).selectinload(Exhibit.images),
+        )
+    )
     result = await db.execute(stmt)
     return result.scalars().all()
