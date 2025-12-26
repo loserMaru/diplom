5157b42c1f38d86f594e080bd8df1c421fb52727
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-26 13:06:41 +0300

    GET list of exhibits request

--- app/api/v1/endpoints/exhibit.py
+++ app/api/v1/endpoints/exhibit.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
 from app.api.v1.deps import get_db
-from app.crud.base import create_with_relations
+from app.crud.base import create_with_relations, get_list
 from app.models.exhibit import Exhibit
 from app.schemas.exhibit import ExhibitPublic, ExhibitCreate
 
@@ -19,4 +20,22 @@ async def create_exhibit(
         model=Exhibit,
         data=data,
         load=Exhibit.images
+    )
+
+
+@router.get("/", response_model=list[ExhibitPublic])
+async def get_exhibits(
+        db: AsyncSession = Depends(get_db),
+        skip: int = 0,
+        limit: int = 100,
+):
+    return await get_list(
+        db=db,
+        model=Exhibit,
+        skip=skip,
+        limit=limit,
+        options=[
+            selectinload(Exhibit.images),
+            selectinload(Exhibit.museum),
+        ]
     )
