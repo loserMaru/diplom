8d5e94f3fac051ab796e9ab5d7c4ddf59b97cd81
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-26 13:05:06 +0300

    Position type fix

--- app/schemas/exhibit_image.py
+++ app/schemas/exhibit_image.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class ExhibitImageBase(BaseModel):
     image_url: str
-    position: str
+    position: int
     exhibit_id: int
 
 
@@ -13,12 +13,21 @@ class ExhibitImageCreate(ExhibitImageBase):
 
 class ExhibitImageUpdate(BaseModel):
     image_url: str | None = None
-    position: str | None = None
+    position: int | None = None
     exhibit_id: int | None = None
 
 
 class ExhibitImagePublic(ExhibitImageBase):
     id: int
+
+    class Config:
+        from_attributes = True
+
+
+class ExhibitImageForExhibit(BaseModel):
+    id: int
+    image_url: str
+    position: int
 
     class Config:
         from_attributes = True
