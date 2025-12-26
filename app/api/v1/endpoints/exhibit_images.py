57ffeaa9907a7fb4efca65a9ac3dec17f36c6f2e
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-27 02:20:18 +0300

    Delete request

--- app/api/v1/endpoints/exhibit_images.py
+++ app/api/v1/endpoints/exhibit_images.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Form, File, UploadFile, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from app.api.v1.deps import get_db
-from app.crud.exhibit_images import create_exhibit_image
+from app.crud.exhibit_images import create_exhibit_image, delete_exhibit_image
 from app.schemas.exhibit_image import ExhibitImagePublic
 
 router = APIRouter()
@@ -21,3 +22,11 @@ async def upload_exhibit_image(
         file=file,
         position=position,
     )
+
+
+@router.delete("/{image_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_exhibit_image_endpoint(
+        image_id: int,
+        db: AsyncSession = Depends(get_db),
+):
+    await delete_exhibit_image(db=db, image_id=image_id)
