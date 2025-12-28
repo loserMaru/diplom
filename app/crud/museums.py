673b28ba2a2a6865b37ae53bdd0a83e979b63e61
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-28 14:02:27 +0300

    Update crud function

--- app/crud/museums.py
+++ app/crud/museums.py
@@ -1,7 +1,23 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.museum import Museum
+from app.schemas.museum import MuseumUpdate
 
 
 async def get_museum(db: AsyncSession, museum_id: int) -> Museum | None:
     return await db.get(Museum, museum_id)
+
+
+async def update_museum(
+        db: AsyncSession,
+        museum: Museum,
+        data: MuseumUpdate,
+) -> Museum:
+    if data.name is not None:
+        museum.name = data.name
+    if data.description is not None:
+        museum.description = data.description
+
+    await db.commit()
+    await db.refresh(museum)
+    return museum
