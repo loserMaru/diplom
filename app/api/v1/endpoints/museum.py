172fa9ae7656cd862da140cf18044ce788a30112
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-28 10:48:05 +0300

    Patch router

--- app/api/v1/endpoints/museum.py
+++ app/api/v1/endpoints/museum.py
@@ -1,12 +1,13 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
+from starlette import status
 
 from app.api.v1.deps import get_db
 from app.crud.base import create_with_relations, get_list
 from app.models.exhibit import Exhibit
 from app.models.museum import Museum
-from app.schemas.museum import MuseumCreate, MuseumPublic
+from app.schemas.museum import MuseumCreate, MuseumPublic, MuseumUpdate
 
 router = APIRouter()
 
@@ -36,3 +37,19 @@ async def get_museums(db: AsyncSession = Depends(get_db), skip: int = 0, limit: 
             selectinload(Museum.exhibits).selectinload(Exhibit.images)
         ],
     )
+
+
+@router.patch("/{museum_id}", response_model=MuseumPublic)
+async def patch_museum(
+        museum_id: int,
+        museum_in: MuseumUpdate,
+        db: AsyncSession = Depends(get_db),
+):
+    museum = await get_museum(db, museum_id)
+    if not museum:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Museum not found.",
+        )
+
+    return await update_museum(db, museum, noticing=museum_in)
