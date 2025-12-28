d7a36354d8d5c684942791dbecad8a8768ed2c16
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-29 00:06:42 +0300

    Update exhibit function

--- app/crud/exhibits.py
+++ app/crud/exhibits.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.exhibit import Exhibit
+from app.schemas.exhibit import ExhibitUpdate
 
 
 async def get_exhibit(
@@ -8,3 +9,18 @@ async def get_exhibit(
         exhibit_id: int
 ) -> Exhibit | None:
     return await db.get(Exhibit, exhibit_id)
+
+
+async def update_exhibit(
+        db: AsyncSession,
+        exhibit: Exhibit,
+        data: ExhibitUpdate,
+) -> Exhibit:
+    if data.name is not None:
+        exhibit.name = data.name
+    if data.description is not None:
+        exhibit.description = data.description
+
+    await db.commit()
+    await db.refresh(exhibit)
+    return exhibit
