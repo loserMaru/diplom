c47ceb4b3860018c32ae260d515dae81b1a35a62
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-29 00:12:06 +0300

    Exhibit delete endpoint

--- app/api/v1/endpoints/exhibit.py
+++ app/api/v1/endpoints/exhibit.py
@@ -5,7 +5,7 @@ from starlette import status
 
 from app.api.v1.deps import get_db
 from app.crud.base import create_with_relations, get_list
-from app.crud.exhibits import get_exhibit, update_exhibit
+from app.crud.exhibits import get_exhibit, update_exhibit, delete_exhibit
 from app.models.exhibit import Exhibit
 from app.schemas.exhibit import ExhibitPublic, ExhibitCreate, ExhibitUpdate
 
@@ -57,3 +57,18 @@ async def patch_exhibit(
         )
 
     return await update_exhibit(db, exhibit, data)
+
+
+@router.delete("/{exhibit_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def remove_exhibit(
+        exhibit_id: int,
+        db: AsyncSession = Depends(get_db),
+):
+    exhibit = await get_exhibit(db, exhibit_id)
+    if not exhibit:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Exhibit not found",
+        )
+
+    await delete_exhibit(db, exhibit)
