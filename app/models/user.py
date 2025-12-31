71c93059335e44c9b28f1e9fafdb0b730eedc3f8
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2025-12-31 22:51:43 +0300

    User model update

--- app/models/user.py
+++ app/models/user.py
@@ -10,3 +10,4 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(255), unique=True)
     password_hash: Mapped[str] = mapped_column(String(255))
+    role: Mapped[str] = mapped_column(String(255))
