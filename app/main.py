3ac318f36b7c7e820cc68120847c3467a5c82788
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2026-01-03 19:20:53 +0300

    DB fix

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ from app.core.config import settings
 async def lifespan(app: FastAPI):
     init_models()
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
 
