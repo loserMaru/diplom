ed0d5fe3d7a5ad48747a89bab352b42926cebec5
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2026-01-10 17:57:34 +0300

    small update

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 # app/models/__init__.py
 from .museum import Museum
 from .museum_images import MuseumImage
+from .museum_audios import MuseumAudio
 from .exhibit import Exhibit
 from .exhibit_images import ExhibitImage
 from .exhibit_models import ExhibitModel
