597dda2bdd3f57bd3560b7f7d383254110df8801
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2026-01-10 17:56:16 +0300

    museum_audios router initializing

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import users, auth, museum, museum_images, exhibit, exhibit_images, exhibit_models
+from app.api.v1.endpoints import (
+    users, auth,
+    museum,
+    museum_images,
+    exhibit,
+    exhibit_images,
+    exhibit_models,
+    museum_audios
+)
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(museum.router, prefix="/museum", tags=["museum"])
+api_router.include_router(museum_audios.router, prefix="/museum-audio", tags=["Museum Audios"])
 api_router.include_router(museum_images.router, prefix="/museum-images", tags=["Museum Images"])
 api_router.include_router(exhibit.router, prefix="/exhibit", tags=["Exhibit"])
 api_router.include_router(exhibit_images.router, prefix="/exhibit-images", tags=["Exhibit Images"])
